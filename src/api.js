4345f626ab635945b2bf0ac751dd2de5d3cc0da0
yaoguangju <yao_guangju@163.com>
2023-05-02 23:47:11 +0800

    初始化文件

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "7d9d25a71bmsh349afe8d392c8f4p180482jsn77a8966687ef",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "29fc3279e580564f96f9f73223f3c2a3"; // enter your key from openweather API
